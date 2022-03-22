9c93ded19af308a2a2f74ea7b9b945f88043a240
Davide Guidetti <73661177+ilmada99@users.noreply.github.com>
2022-03-22 09:20:50 +0100

    fix

--- it.unibo.comm2022/src/it/unibo/comm2022/utils/CommSystemConfig.java
+++ it.unibo.comm2022/src/it/unibo/comm2022/utils/CommSystemConfig.java
@@ -22,7 +22,7 @@ public class CommSystemConfig {
 	}
 	
 	public static void setTheConfiguration( String resourceName ) {
-		//Nella distribuzione resourceName è in una dir che include la bin  
+		//Nella distribuzione resourceName ï¿½ in una dir che include la bin  
 		FileInputStream fis = null;
 		try {
 			ColorsOut.out("%%% setTheConfiguration from file:" + resourceName);
