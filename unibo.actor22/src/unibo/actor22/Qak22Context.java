c33752f6cece07f6e772d049f71d61a611d85340
Davide Guidetti <73661177+ilmada99@users.noreply.github.com>
2022-04-07 13:45:54 +0200

    format

--- unibo.actor22/src/unibo/actor22/Qak22Context.java
+++ unibo.actor22/src/unibo/actor22/Qak22Context.java
@@ -9,75 +9,68 @@ import unibo.actor22comm.proxy.ProxyAsClient;
 import unibo.actor22comm.utils.ColorsOut;
 import unibo.actor22comm.utils.CommUtils;
 
- 
-
 public class Qak22Context {
-	private static HashMap<String,QakActor22> ctxMap      = new HashMap<String,QakActor22>();
-    private static HashMap<String,ProxyAsClient> proxyMap = new HashMap<String,ProxyAsClient>();
+	private static HashMap<String, QakActor22> ctxMap = new HashMap<String, QakActor22>();
+	private static HashMap<String, ProxyAsClient> proxyMap = new HashMap<String, ProxyAsClient>();
 
-    public static final String actorReplyPrefix = "arp_";
-    
-    public static void setActorAsRemote(
-    		String actorName, String entry, String host, ProtocolType protocol ) {
-    		if( ! proxyMap.containsKey(actorName+"Pxy")) { //un solo proxy per contesto remoto
-	    		ProxyAsClient pxy = new ProxyAsClient(actorName+"Pxy", host, entry, protocol);
-	    		proxyMap.put(actorName, pxy);
-    		}
-    }
-    
+	public static final String actorReplyPrefix = "arp_";
 
-	public static void addActor(QakActor22 a) {	
-        ctxMap.put(a.getName(), a);
-    }
-	public static void removeActor(QakActor22 a) {	
-        ctxMap.remove( a.getName() );
-    }	
-    public static QakActor22 getActor(String actorName) {
-        return ctxMap.get(actorName);
-    }
-    
+	public static void setActorAsRemote(String actorName, String entry, String host, ProtocolType protocol) {
+		if (!proxyMap.containsKey(actorName + "Pxy")) { // un solo proxy per contesto remoto
+			ProxyAsClient pxy = new ProxyAsClient(actorName + "Pxy", host, entry, protocol);
+			proxyMap.put(actorName, pxy);
+		}
+	}
+
+	public static void addActor(QakActor22 a) {
+		ctxMap.put(a.getName(), a);
+	}
+
+	public static void removeActor(QakActor22 a) {
+		ctxMap.remove(a.getName());
+	}
+
+	public static QakActor22 getActor(String actorName) {
+		return ctxMap.get(actorName);
+	}
+
 //proxy
- 
+
 	public static ProxyAsClient getProxy(String actorName) {
-        return proxyMap.get(actorName);
-    }
+		return proxyMap.get(actorName);
+	}
 
-	
-	
 //Annotations
-	
-    public static void handleLocalActorDecl(Object element) {
-    	AnnotUtil.createActorLocal(element);
-    }
-    public static void handleRemoteActorDecl(Object element) {
-     	AnnotUtil.createProxyForRemoteActors(element);
-    }
 
-    public static void handleActorDeclaration(Object element) {
-    	AnnotUtil.createActorLocal(element);
-    	AnnotUtil.createProxyForRemoteActors(element);
-    }
-   
+	public static void handleLocalActorDecl(Object element) {
+		AnnotUtil.createActorLocal(element);
+	}
+
+	public static void handleRemoteActorDecl(Object element) {
+		AnnotUtil.createProxyForRemoteActors(element);
+	}
+
+	public static void handleActorDeclaration(Object element) {
+		AnnotUtil.createActorLocal(element);
+		AnnotUtil.createProxyForRemoteActors(element);
+	}
+
 //Events
-    
-    public static final String registerForEvent   = "registerForEvent";
-    public static final String unregisterForEvent = "unregisterForEvent";
-    
-	
+
+	public static final String registerForEvent = "registerForEvent";
+	public static final String unregisterForEvent = "unregisterForEvent";
+
 	public static void registerAsEventObserver(String observer, String evId) {
 		QakActor22 a = getActor(EventMsgHandler.myName);
-		if( a == null ) new EventMsgHandler();
-		IApplMessage m =
-				CommUtils.buildDispatch(observer, registerForEvent, evId, EventMsgHandler.myName);
-		Qak22Util.sendAMsg( m, EventMsgHandler.myName );  //Redirection to store
+		if (a == null)
+			new EventMsgHandler();
+		IApplMessage m = CommUtils.buildDispatch(observer, registerForEvent, evId, EventMsgHandler.myName);
+		Qak22Util.sendAMsg(m, EventMsgHandler.myName); // Redirection to store
 	}
-	
-	public static void unregisterAsEventObserver(String observer, String evId) {
-		IApplMessage m =
-				CommUtils.buildDispatch(observer, unregisterForEvent, evId, EventMsgHandler.myName);
-		Qak22Util.sendAMsg( m, EventMsgHandler.myName );  //Redirection to store
-	}
-	
 
-    
+	public static void unregisterAsEventObserver(String observer, String evId) {
+		IApplMessage m = CommUtils.buildDispatch(observer, unregisterForEvent, evId, EventMsgHandler.myName);
+		Qak22Util.sendAMsg(m, EventMsgHandler.myName); // Redirection to store
+	}
+
 }
--- unibo.actor22/src/unibo/actor22comm/context/ContextMsgHandler.java
+++ unibo.actor22/src/unibo/actor22comm/context/ContextMsgHandler.java
@@ -11,42 +11,44 @@ import unibo.actor22comm.interfaces.IApplMsgHandler;
 import unibo.actor22comm.interfaces.Interaction2021;
 import unibo.actor22comm.utils.ColorsOut;
 
-
-
 /*
  */
 
-public class ContextMsgHandler extends ApplMsgHandler implements IApplMsgHandler{ 
+public class ContextMsgHandler extends ApplMsgHandler implements IApplMsgHandler {
 //	protected HashMap<String,IApplMsgHandler> handlerMap = new HashMap<String,IApplMsgHandler>(); //OLD
 
- 	
 	public ContextMsgHandler(String name) {
 		super(name);
- 	}
+	}
 
 	@Override
-	public void elaborate( IApplMessage msg, Interaction2021 conn ) {
-		ColorsOut.out(name+" | elaborateee" + msg + " conn=" + conn, ColorsOut.GREEN);
-		//msg( MSGID, MSGTYPE, SENDER, RECEIVER, CONTENT, SEQNUM )		
-     	if( msg.isRequest() ) elabRequest(msg,conn);
-     	else  elabNonRequest(msg,conn);
+	public void elaborate(IApplMessage msg, Interaction2021 conn) {
+		ColorsOut.out(name + " | elaborateee" + msg + " conn=" + conn, ColorsOut.GREEN);
+		// msg( MSGID, MSGTYPE, SENDER, RECEIVER, CONTENT, SEQNUM )
+		if (msg.isRequest())
+			elabRequest(msg, conn);
+		else
+			elabNonRequest(msg, conn);
 	}
-	
-	protected void elabRequest( IApplMessage msg, Interaction2021 conn ) {
+
+	protected void elabRequest(IApplMessage msg, Interaction2021 conn) {
 		String senderName = msg.msgSender();
-		//Attivo un attore per ricevere la risposta
-		String actorRepyName = Qak22Context.actorReplyPrefix+senderName;
-		if( Qak22Context.getActor(actorRepyName) == null ) { //non esiste già
+		// Attivo un attore per ricevere la risposta
+		String actorRepyName = Qak22Context.actorReplyPrefix + senderName;
+		if (Qak22Context.getActor(actorRepyName) == null) { // non esiste giï¿½
 			ColorsOut.out(name + " | CREATE ACTOR FOR REPLY " + msg, ColorsOut.BLUE);
 			new ActorForReply(actorRepyName, this, conn);
-		}		
-		elabNonRequest(msg,conn);
- 	}
-	protected void elabNonRequest( IApplMessage msg, Interaction2021 conn ) {
+		}
+		elabNonRequest(msg, conn);
+	}
+
+	protected void elabNonRequest(IApplMessage msg, Interaction2021 conn) {
 		String receiver = msg.msgReceiver();
 		QakActor22 a = Qak22Context.getActor(receiver);
-		if( a != null ) { Qak22Util.sendAMsg( msg ); }		
-		else ColorsOut.outerr(name + " | actor " + receiver + " non local (I should not be here) ");
+		if (a != null) {
+			Qak22Util.sendAMsg(msg);
+		} else
+			ColorsOut.outerr(name + " | actor " + receiver + " non local (I should not be here) ");
 	}
-	
+
 }
